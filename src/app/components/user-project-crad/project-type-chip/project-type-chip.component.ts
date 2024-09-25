0a6b0275e51b1d01bd55176013004f2099267207
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-25 23:23:54 +0300

    Created ProjectTypeChip component

--- src/app/components/user-project-crad/project-type-chip/project-type-chip.component.ts
+++ src/app/components/user-project-crad/project-type-chip/project-type-chip.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'app-project-type-chip',
+  selector: 'ds-project-type-chip',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './project-type-chip.component.html',
-  styleUrl: './project-type-chip.component.scss'
+  styleUrl: './project-type-chip.component.scss',
 })
 export class ProjectTypeChipComponent {
-
+  @Input() projectType: 'web-app' | 'game' | 'default' = 'default';
 }
