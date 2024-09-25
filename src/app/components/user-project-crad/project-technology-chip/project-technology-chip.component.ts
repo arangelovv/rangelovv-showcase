62aa8aad668521963dd2db0a6fb18c953a8fac3f
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-25 23:23:33 +0300

    Created ProjectTechChip component

--- src/app/components/user-project-crad/project-technology-chip/project-technology-chip.component.ts
+++ src/app/components/user-project-crad/project-technology-chip/project-technology-chip.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'app-project-technology-chip',
+  selector: 'ds-project-technology-chip',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './project-technology-chip.component.html',
-  styleUrl: './project-technology-chip.component.scss'
+  styleUrl: './project-technology-chip.component.scss',
 })
 export class ProjectTechnologyChipComponent {
-
+  @Input() tech: 'Angular' | 'CSS' | 'Firebase' | 'Tailwind' | 'default' =
+    'default';
 }
